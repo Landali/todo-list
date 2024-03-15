9a428eed087a65b018828b7d37a1b04c1a41f183
allan-paz <allan.paz@laureate.net>
2024-03-15 03:17:04 -0600

    added fix to todolist component

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -10,7 +10,7 @@ export const TodoList = ({ list, deleteTodo, editTodo, updateTodo }) => {
             {list.map((todo, index) =>
                 todo.isEditing ? (
                     <EditTodoForm classname={'form'} updateTodo={updateTodo} todo={todo} index={index} buttonText={'Edit Todo'} />
-                ) : (<TodoItem status={todo.status} todo={todo.todo} priority={todo.priority} editTodo={editTodo} deleteTodo={deleteTodo} index={index}/>)
+                ) : (<TodoItem status={todo.status} todo={todo.todo} priority={todo.priority} editTodo={editTodo} deleteTodo={deleteTodo} index={index} />)
             )
             }
 
